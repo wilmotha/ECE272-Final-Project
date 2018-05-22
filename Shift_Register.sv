32d875f14a6eee81eb617f1e5d74360a3bc88320
gs291 <sanchez.gregory886@gmail.com>
2018-05-22 15:35:48 -0700

    Add initial Shift_Register.sv

---
+++ Shift_Register.sv
@@ -0,0 +1,18 @@
+module shiftreg #(parameter N = 4)
+				 (input logic 			clk,
+				  input logic 			reset, load,
+				  input logic 			sin,
+				  input logic [N–1:0] 	d,
+				  output logic [N–1:0] 	q,
+				  output logic 			sout);
+					  
+	always_ff @(posedge clk, posedge reset)
+		if (reset) 
+			q <= 0;
+		else if (load) 
+			q <= d;
+		else 
+			q <= {q[N–2:0], sin};
+			
+	assign sout = q[N–1];
+endmodule
