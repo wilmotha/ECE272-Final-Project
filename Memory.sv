7b7caaae7ef9504c39e8a50d594ee5d3528d0c0b
gs291 <sanchez.gregory886@gmail.com>
2018-05-25 00:48:27 -0700

    Fixed type mismatch bug

--- Memory.sv
+++ Memory.sv
@@ -2,15 +2,15 @@ module ram	(input logic 			clk_manual,
 			 input logic 			reset_n,
 			 input logic 			we, 	//Write Enable
 			 input logic [2:0] 		adr,	//Address
-			 input logic [3:0] 		value,
-			 output logic [3:0] 	dout,
+			 input logic [4:0] 		value,
+			 output logic [4:0] 	dout,
 			 output logic [2:0] 	adr_next);
 			 
-	logic [3:0] mem [2**2:0];	//No clue what this is
+	logic [4:0] mem [2**3-1:0];	//No clue what this is
 	
 	always_ff @(posedge clk_manual, negedge reset_n)
 		if (!reset_n)
-			mem <= 0;
+			mem <= {mem[6:0], 7'b0};
 		else if (we) 
 			begin
 				mem [adr] <= value;
