a022598861d9953f348b017cbe0e592951219df9
gs291 <sanchez.gregory886@gmail.com>
2018-06-03 21:15:14 -0700

    Update to more advacned clock counter

--- Clock_Counter.sv
+++ Clock_Counter.sv
@@ -1,26 +1,18 @@
 module clock_counter(
-	input logic clk_i,		//often, "tags" are added to variables to denote what they do for the user
-	input logic reset_n,	//here, 'i' is used for input and 'o' for the output, while 'n' specifies
-												//an active low signal ("not")
-	output logic clk_o);
+  input logic clk_i,	// the clock driving the counter
+  input logic reset_n,		// active low reset	
+  
+  output logic clk_o,
+  output logic clock_1Hz
+  ); 
 
-		logic [16:0] count;	//register stores the counter value so that it can be modified
-					//on a clock edge. Register size needs to store as large of a
-					//number as the counter reaches. Here, 2^(16+1) = 131,072.
+  logic [20:0] count;
 
-		always_ff @ (posedge clk_i, negedge reset_n)
-			begin
-				count <= count + 1;	//at every positive edge, the counter is increased by 1
-				if(!reset_n) //If reset_n gets pulled to ground (active low), reset count to 0
-					begin
-						clk_o <= 0;
-						count <= 0;
-					end
-				else
-					if(count >= 100) //Flips the slow clock every 100000 clock cycles
-						begin
-							clk_o <= ~clk_o;	//Flip slow clock
-							count <= 0;			//Reset the counter
-						end
-			end
+  always_ff@(posedge clk_i, negedge reset_n)
+    if   (!reset_n)	count <= 0;
+    else 	   	count <= count + 1;
+
+  assign clk_o = count[10];
+  assign clock_1Hz = count[17]; 
+
 endmodule
